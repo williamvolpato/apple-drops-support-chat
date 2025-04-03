ec16046457d3f0e8c35b7b2ecac8289e0d9bcb19
William Volpato <william.volpato@hotmail.com>
2025-04-03 19:50:21 -0300

    Create route.ts

---
+++ app/api/messages/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  const messages = [
+    {
+      sender: '+12523903613',
+      text: 'Hi, I received a message and I’m interested.',
+    },
+    {
+      sender: 'Suporte',
+      text: 'Hi! I can help you complete your order.',
+    },
+  ]
+
+  return NextResponse.json(messages)
+}
